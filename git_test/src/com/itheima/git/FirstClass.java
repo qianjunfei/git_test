1252241591ce4e033f3f39daf1344386cac15369
qianjunfei <791199083@qq.com>
2018-12-27 09:23:18 +0800

    1234

---
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("双击666");
+        System.out.println("双击666");
+        System.out.println("双击666");
+        System.out.println("双击666");
+        System.out.println("双击666");
+        System.out.println("双击666");
+        System.out.println("双击666");
+    }
+
+    public void add(){
+        int a=0;
+        int b=0;
+    }
+}
